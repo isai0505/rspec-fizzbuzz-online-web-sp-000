535deccc9a57ccab64e6c8f6f0d28b30f59bcd17
Isai Bismark Cortez <isai0505@gmail.com>
2019-06-26 15:08:56 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,7 +3,7 @@
 def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
-  elsif int % 5 == 0
-    "Buzz"
+  elsif int % 5 == 0 and int % 3 == 0
+    "FizzBuzz"
   end
 end
